aa2cb924b34f810d9621503f53fc8f493d2cd19b
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2020-07-31 19:26:50 -0400

    add TypedString and tests

---
+++ StringThings.UnitTests/TypedStringTestFixture.cs
@@ -0,0 +1,56 @@
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace StringThings.UnitTests
+{
+    public class TypedStringTestFixture
+    {
+        public class TestTypedString : TypedString
+        {
+            public static TestTypedString A => new TestTypedString("A");
+            public static TestTypedString AnotherA => new TestTypedString("A");
+            public static TestTypedString B => new TestTypedString("B");
+
+            protected TestTypedString(string? value) : base(value) { }
+
+            public static TypedString NullValue => new TestTypedString(null);
+        }
+
+        [Fact]
+        public void WhenValuesAreEqual_ThenObjectsAreEqual()
+        {
+            TestTypedString.A.Should().Be(TestTypedString.AnotherA);
+            TestTypedString.A.Equals(TestTypedString.AnotherA).Should().BeTrue();
+            (TestTypedString.A == TestTypedString.AnotherA).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenValuesAreNotEqual_ThenObjectsAreNotEqual()
+        {
+            TestTypedString.A.Should().NotBe(TestTypedString.B);
+            TestTypedString.A.Equals(TestTypedString.B).Should().BeFalse();
+            (TestTypedString.A != TestTypedString.AnotherA).Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenBothNull_ThenEqual()
+        {
+            (null == TestTypedString.NullValue).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenToStrung_ThenReturnsValue()
+        {
+            ((string?)TestTypedString.A).Should().Be(TestTypedString.A.ToString());
+            TestTypedString.NullValue.ToString().Should().Be(string.Empty);
+        }
+
+        [Fact]
+        public void WhenLinqed_ThenEqualityWorks()
+        {
+            var typedStrings = new TestTypedString[] { TestTypedString.A, TestTypedString.AnotherA, TestTypedString.B, TestTypedString.B };
+            typedStrings.Distinct().Count().Should().Be(2);
+        }
+    }
+}
---
+++ StringThings/TypedString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringThings
+{
+    public abstract class TypedString : IEquatable<TypedString>
+    {
+        private readonly string? _value;
+
+        protected TypedString(string? value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator string?(TypedString? typedString) => typedString?._value;
+        public static bool operator ==(TypedString? a, TypedString? b) => Equals(a, b);
+        public static bool operator !=(TypedString? a, TypedString? b) => !Equals(a, b);
+
+        public override bool Equals(object? obj) => obj is TypedString && Equals(obj as TypedString);
+
+        public bool Equals(TypedString? other) => Equals(this, other);
+
+        public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+
+        public override string ToString() => _value ?? string.Empty;
+
+        private static bool Equals(TypedString? a, TypedString? b) => string.Equals(a, b);
+    }
+}
