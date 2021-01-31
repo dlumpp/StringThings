31ef297a91b3af17a7723b7fca4581b354802adb
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2021-01-31 17:35:41 -0500

    add EqualsAny Extensions #5

---
+++ StringThings.UnitTests/Extensions/EqualsAnyExtensionsTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using StringThings.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace StringThings.UnitTests.Extensions
+{
+    public class EqualsAnyExtensionsTests
+    {
+        protected virtual (bool ParamsActual, bool EnumerableActual) Act(string? s, IEnumerable<string?>? options)
+        {
+            var paramsActual = s.EqualsAny(options?.ToArray());
+            var enumerableActual = s.EqualsAny(options?.ToList());
+            return (paramsActual, enumerableActual);
+        }
+
+        protected void Assert((bool ParamsActual, bool EnumerableActual) actual, bool expected)
+        {
+            actual.ParamsActual.Should().Be(expected);
+            actual.EnumerableActual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void WhenOptionsNull_ThenFalse()
+        {
+            List<string>? options = null;
+            var actual = Act("", options);
+            Assert(actual, false);
+        }
+
+        [Fact]
+        public void WhenStringNotEqualsAnyOptions_ThenFalse()
+        {
+            var options = new List<string> { "A", "B" };
+            var actual = Act("F", options);
+            Assert(actual, false);
+        }
+
+        [Fact]
+        public void WhenStringIsNull_AndNullInOptions_ThenTrue()
+        {
+            var options = new List<string?> { "A", null };
+            var actual = Act(null, options);
+            Assert(actual, true);
+        }
+
+        [Fact]
+        public void WhenStringEqualsAnyOptions_ThenTrue()
+        {
+            var options = new List<string> { "A", "B" };
+            var actual = Act("A", options);
+            Assert(actual, true);
+        }
+    }
+
+    public class EqualsAnyIgnoreCaseExtensionsTests : EqualsAnyExtensionsTests
+    {
+        protected override (bool ParamsActual, bool EnumerableActual) Act(string? s, IEnumerable<string?>? options)
+        {
+            var paramsActual = s.EqualsAnyIgnoreCase(options?.ToArray());
+            var enumerableActual = s.EqualsAnyIgnoreCase(options?.ToList());
+            return (paramsActual, enumerableActual);
+        }
+
+        [Fact]
+        public void WhenStringEqualsAnyOptionsOfDifferentCase_ThenTrue()
+        {
+            var options = new List<string> { "A", "B" };
+            var actual = Act("a", options);
+            Assert(actual, true);
+        }
+    }
+}
---
+++ StringThings/Extensions/EqualsAnyExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringThings.Extensions
+{
+    public static class EqualsAnyExtensions
+    {
+        public static bool EqualsAny(this string? s, params string?[]? options) =>
+            EqualsAny(s, options?.ToList());
+
+        public static bool EqualsAny(this string? s, IEnumerable<string?>? options) =>
+            EqualsAny(s, options?.ToList());
+
+        private static bool EqualsAny(this string? s, IList<string?>? options) =>
+            options?.Contains(s) ?? false;
+
+        public static bool EqualsAnyIgnoreCase(this string? s, params string?[]? options) =>
+            EqualsAnyIgnoreCase(s, options?.ToList());
+
+        public static bool EqualsAnyIgnoreCase(this string? s, IEnumerable<string?>? options) =>
+            EqualsAnyIgnoreCase(s, options?.ToList());
+
+        private static bool EqualsAnyIgnoreCase(this string? s, IList<string?>? options) =>
+            options?.Any(i =>
+            {
+                if (s == null && i == null)
+                    return true;
+                return s?.EqualsIgnoreCase(i) ?? false;
+            }) ?? false;
+    }
+}
